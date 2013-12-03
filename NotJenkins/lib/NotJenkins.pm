2acf7ed8f5c7427e9738635cc570ae6bd02e7423
James Aitken <loonypandora@gmail.com>
2013-12-03 15:14:21 +0000

    Actually get some data

--- NotJenkins/lib/NotJenkins.pm
+++ NotJenkins/lib/NotJenkins.pm
@@ -4,6 +4,8 @@ use Dancer ":syntax";
 use Dancer::Plugin::Database;
 use Data::Dump qw(dump);
 
+use JSON;
+
 use common::sense;
 
 
@@ -11,16 +13,26 @@ use common::sense;
 get "/NotJenkins/builds" => sub {
 
     my $sth = database->prepare(q{
-        SELECT * FROM pull_requests
+        SELECT pull_requests.id, github_number, github_title, github_state, github_created_at, github_updated_at, display_title, success
+        FROM pull_requests
         LEFT JOIN projects ON project_id = projects.id
-        LEFT JOIN builds ON pull_requests.id = pull_id
-        ORDER BY github_updated_at, github_created_at DESC
+        LEFT JOIN (SELECT id, pull_id, success FROM builds ORDER BY id DESC LIMIT 1) AS builds ON builds.pull_id = pull_requests.id
+        ORDER BY github_updated_at DESC, github_created_at DESC
     });
 
     $sth->execute();
 
+    my $result = $sth->fetchall_arrayref({});
+
+    # Numify `github_number`, `id` & truthify `success`
+    for my $pr (@$result) {
+        $pr->{id} += 0;
+        $pr->{github_number} += 0;
+        $pr->{success} = \1 if $pr->{success};
+    }
+
     return {
-        builds => $sth->fetchrow_hashref
+        builds => $result
     }
 };
 
